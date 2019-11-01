c02e032306ecce49d8890320123d5c20cc5a596c
ahmad mohammad alizadeh <38861627+sparof@users.noreply.github.com>
2019-11-01 20:11:49 +0330

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,68 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+var color1 =document.querySelector(".color1");
+var color2 =document.querySelector(".color2");
+var mydiv =document.getElementById("myDIV");
+var myul =document.getElementById("myUL");
+
+color1.addEventListener("input",function(){
+  console.log(color1.value)
+  mydiv.style.background = " linear-gradient(to right, "+color1.value+", " + color2.value+")";
+})
+
+color2.addEventListener("input",function(){
+  mydiv.style.background = " linear-gradient(to right, "+color1.value+", " + color2.value+")";
+})
